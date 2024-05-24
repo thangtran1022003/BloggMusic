1a265b995f0ba7dab62f52f1dfe88c9ed4733687
thangtran1022003 <tthang679@gmail.com>
2024-05-24 13:25:42 +0700

    first commit

---
+++ src/playlist.js
@@ -0,0 +1,198 @@
+import { v4 as uuidv4 } from "uuid";
+import howyoulikethat from '@types/connect/HYLT.mp3';
+import pinkvenom from '@types/connect/PV.mp3';
+import shutdown from '@types/connect/SD.mp3';
+import killthislove from '@types/connect/KTL.mp3';
+import duududuudu from '@types/connect/DDDD.mp3';
+import lovesickgirl from '@types/connect/LSG.mp3';
+import tally from '@types/connect/T.mp3';
+import typagirl from '@types/connect/TYG.mp3';
+import prettysavage from '@types/connect/PS.mp3';
+import foreveryoung from '@types/connect/FY.mp3';
+import thegirl from '@types/connect/TG.mp3';
+import boombayah from '@types/connect/BBA.mp3';
+import playingwithfire from '@types/connect/PWF.mp3';
+import whistle from '@types/connect/W.mp3';
+import readyforlove from '@types/connect/RFL.mp3';
+import flower from '@types/connect/F.mp3';
+import money from '@types/connect/M.mp3';
+import solo from '@types/connect/S.mp3';
+import gone from '@types/connect/G.mp3';
+function chillHop() {
+  return [
+    {
+      name: "How You Like That",
+      cover: "https://i1.sndcdn.com/artworks-uNy9GKMXK2bC9Qah-XPqymg-t500x500.jpg",
+      artist: "BlackPink",
+      audio: howyoulikethat,
+      color: ["#47609D", "#E88774"],
+      id: uuidv4(),
+      active: true,
+    },
+    {
+      name: "Pink Venom",
+      cover:"https://lahstalon.org/wp-content/uploads/2022/09/IMG_8691.png",
+      artist: "BlackPink",
+      audio: pinkvenom,
+      color: ["#205950", "#2ab3bf"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Shut Down",
+      cover: "https://i.pinimg.com/736x/30/c1/cd/30c1cd2d7e0c379335d1757405624133.jpg",
+      artist: "BlackPink",
+      audio: shutdown,
+      color: ["#EF8EA9", "#ab417f"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Kill This Love",
+      cover: "https://image.sggp.org.vn/w1000/Uploaded/2024/dureixrxkw/2019_12_29/kill-this-love_sggp_MRRA.jpg.webp",
+      artist: "BlackPink",
+      audio: killthislove,
+      color: ["#CD607D", "#c94043"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "DDU-DU-DUU-DU",
+      cover:"https://media1.nguoiduatin.vn/thumb_x600x600/media/tran-danh-tuyen/2019/01/23/blackpink-nguoiduatin.jpg",
+      artist: "BlackPink",
+      audio: duududuudu,
+      color: ["#335561", "#3A393E"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "LoveSick Girls",
+      cover:"https://i1.sndcdn.com/artworks-yUsXOldxNzqKdPe4-918K8Q-t500x500.jpg",
+      artist: "BlackPink",
+      audio: lovesickgirl,
+      color: ["#2A416D", "#E98087"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Tally",
+      cover:"https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/680681ea-76ed-4371-8170-ee2d389a49be/dfoaze2-500434a6-8ead-4bea-a46c-6a7f9732dffd.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzY4MDY4MWVhLTc2ZWQtNDM3MS04MTcwLWVlMmQzODlhNDliZVwvZGZvYXplMi01MDA0MzRhNi04ZWFkLTRiZWEtYTQ2Yy02YTdmOTczMmRmZmQuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.esK8E9bx21tvS6LW4h8Tw2XM7AbE5A9KQySzaJuhf6U",
+      artist: "BlackPink",
+      audio: tally,
+      color: ["#BD3D76", "#551853"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Typa Girl",
+      cover:"https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/0fc9ded4-899e-408f-8b33-c893599d0397/dffqoi4-296bbf64-801b-4e5a-b76e-df93937c1402.png/v1/fill/w_894,h_894,q_70,strp/blackpink___typa_girl_by_neonflowerdesigns_dffqoi4-pre.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzBmYzlkZWQ0LTg5OWUtNDA4Zi04YjMzLWM4OTM1OTlkMDM5N1wvZGZmcW9pNC0yOTZiYmY2NC04MDFiLTRlNWEtYjc2ZS1kZjkzOTM3YzE0MDIucG5nIiwiaGVpZ2h0IjoiPD0xMDI0Iiwid2lkdGgiOiI8PTEwMjQifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uud2F0ZXJtYXJrIl0sIndtayI6eyJwYXRoIjoiXC93bVwvMGZjOWRlZDQtODk5ZS00MDhmLThiMzMtYzg5MzU5OWQwMzk3XC9uZW9uZmxvd2VyZGVzaWducy00LnBuZyIsIm9wYWNpdHkiOjk1LCJwcm9wb3J0aW9ucyI6MC40NSwiZ3Jhdml0eSI6ImNlbnRlciJ9fQ.twe6nPv7VJV2UtlZk-d2GgQM4WlUuPSMtAQNF6XP4Ag",
+      artist: "BlackPink",
+      audio: typagirl,
+      color: ["#BD3D76", "#551853"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Pretty Savage",
+      cover:"https://i1.sndcdn.com/artworks-CLNfh7ZXUNVOCpFe-NF7Iuw-t500x500.jpg",
+      artist: "BlackPink",
+      audio: prettysavage,
+      color: ["#F2C5AB", "#BE5853"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Forever Young",
+      cover:"https://i1.sndcdn.com/artworks-EzhBn3SangIQdebR-3EekyA-t500x500.jpg",
+      artist: "BlackPink",
+      audio: foreveryoung,
+      color: ["#FF194A", "#41B3B3"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "The Girls",
+      cover:"https://avatar-ex-swe.nixcdn.com/song/2023/08/25/b/5/0/5/1692944545573_640.jpg",
+      artist: "BalckPink",
+      audio: thegirl,
+      color: ["#CA483B", "#682C24"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Boombayah",
+      cover:"https://i.pinimg.com/originals/70/e2/68/70e268542c426592b9a7ad37427106ef.jpg",
+      artist: "BlackPink",
+      audio: boombayah,
+      color: ["#EF8EA9", "#ab417f"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Playing With Fire",
+      cover:"https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/3f5ac8ed-8ec1-40c3-aa93-d74b7c37e501/ddjakk7-4ef55186-f8e5-4382-b634-735ea9bb6eef.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzNmNWFjOGVkLThlYzEtNDBjMy1hYTkzLWQ3NGI3YzM3ZTUwMVwvZGRqYWtrNy00ZWY1NTE4Ni1mOGU1LTQzODItYjYzNC03MzVlYTliYjZlZWYuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.Yack5yBP7mpUEg4xdH60G5KNDymw39piLnoDj4yp52I",
+      artist: "BlackPink",
+      audio: playingwithfire,
+      color: ["#CD607D", "#c94043"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Whistle",
+      cover:"https://kenh14cdn.com/203336854389633024/2021/8/16/photo-1-1629121648656283506628.jpg",
+      artist: "BlackPink",
+      audio: whistle,
+      color: ["#90B4AA", "#F2AE87"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Ready For Love",
+      cover:"https://i1.sndcdn.com/artworks-3sgMDWHFyvkK4oVT-T9SfaQ-t500x500.jpg",
+      artist: "BlackPink",
+      audio: readyforlove,
+      color: ["#B442A1", "#F54F7A"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: " Flowers",
+      cover:"https://dep.com.vn/wp-content/uploads/2023/04/337315764_129412266751242_6983341296233163086_n.jpg",
+      artist: "Jisoo for BlackPink",
+      audio: flower,
+      color: ["#A35CA0", "#EE8D66"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Monney",
+      cover:"https://i.scdn.co/image/ab67616d0000b273330f11fb125bb80b760f9e19",
+      artist: "Lisa for BlackPink",
+      audio: money,
+      color: ["#205950", "#2ab3bf"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Solo",
+      cover: "https://m.media-amazon.com/images/M/MV5BZDNlNjg4NTUtOTEwYi00NjJjLThkZWEtNTBkOGZjNGIyY2VkXkEyXkFqcGdeQXVyMTI1Mzg0ODA5._V1_.jpg",
+      artist: "Jennie for BlackPink",
+      audio: solo,
+      color: ["#74c7b4", "#1d6d92"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Gone",
+      cover:"https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/bcc96d39-785d-4796-bb89-fede2ff7fe12/dfg6ln8-58ea2670-6b85-4aed-b862-b88b026d13dd.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2JjYzk2ZDM5LTc4NWQtNDc5Ni1iYjg5LWZlZGUyZmY3ZmUxMlwvZGZnNmxuOC01OGVhMjY3MC02Yjg1LTRhZWQtYjg2Mi1iODhiMDI2ZDEzZGQuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.mntzK-I4I8Uy4NZLzK_CIGnPfkX4yKWhtdhYXXHy3P4",
+      artist: "Roses for BlackPink",
+      audio: gone,
+      color: ["#C668B2", "#4D92D7"],
+      id: uuidv4(),
+      active: false,
+    },
+
+    //ADD MORE HERE
+  ];
+}
+export default chillHop;
